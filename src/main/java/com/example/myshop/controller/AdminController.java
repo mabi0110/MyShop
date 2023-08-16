030702db9157ca490cde098a986e3a8859cc00df
mabi0110 <biziewskamk@gmail.com>
2023-08-16 16:14:38 +0200

    add addItem method

--- src/main/java/com/example/myshop/controller/AdminController.java
+++ src/main/java/com/example/myshop/controller/AdminController.java
@@ -1,7 +1,9 @@
 package com.example.myshop.controller;
 
+import com.example.myshop.model.Item;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +13,11 @@ public class AdminController {
     @GetMapping
     private String adminPage() {
         return "adminview/adminPage";
+    }
+
+    @PostMapping
+    private String addItem(Item item) {
+        HomeController.items.add(item);
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/myshop/controller/HomeController.java
+++ src/main/java/com/example/myshop/controller/HomeController.java
@@ -7,19 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HomeController {
 
-    private List<Item> items = List.of(
-            new Item("Długopis", new BigDecimal("3.8"), "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqagQWzJRwvotqcrZD5TZhk5eqE1QvPJ7dAYHiLXhrcWyeHiq-YA46S57PVnf5wR_-Bmg&usqp=CAU"),
-            new Item("Długopis", new BigDecimal("3.8"), "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqagQWzJRwvotqcrZD5TZhk5eqE1QvPJ7dAYHiLXhrcWyeHiq-YA46S57PVnf5wR_-Bmg&usqp=CAU"),
-            new Item("Pióro", new BigDecimal("12.6"), "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAatvszFV6LIDGoo_JIVXZRONRpzYDExm3uw&usqp=CAU"),
-            new Item("Ołówek", new BigDecimal("1.5"), "https://iheartcraftythings.com/wp-content/uploads/2021/05/Pencil-DRAWING-%E2%80%93-STEP-10.jpg"),
-            new Item("Ołówek", new BigDecimal("1.5"), "https://iheartcraftythings.com/wp-content/uploads/2021/05/Pencil-DRAWING-%E2%80%93-STEP-10.jpg"),
-            new Item("Ołówek", new BigDecimal("1.5"), "https://iheartcraftythings.com/wp-content/uploads/2021/05/Pencil-DRAWING-%E2%80%93-STEP-10.jpg")
-    );
+    static List<Item> items = new ArrayList<>();
+
+    static {
+        items.add(new Item("Długopis", new BigDecimal("3.8"), "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqagQWzJRwvotqcrZD5TZhk5eqE1QvPJ7dAYHiLXhrcWyeHiq-YA46S57PVnf5wR_-Bmg&usqp=CAU"));
+        items.add(new Item("Pióro", new BigDecimal("12.6"), "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAatvszFV6LIDGoo_JIVXZRONRpzYDExm3uw&usqp=CAU"));
+        items.add(new Item("Ołówek", new BigDecimal("1.5"), "https://iheartcraftythings.com/wp-content/uploads/2021/05/Pencil-DRAWING-%E2%80%93-STEP-10.jpg"));
+    }
+
 
     @GetMapping("/")
     public String home(Model model) {
