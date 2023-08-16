971f2bc9dd75c10590ae94890eea597355aa4218
mabi0110 <biziewskamk@gmail.com>
2023-08-16 12:32:25 +0200

    add model and other shop pages

--- src/main/java/com/example/myshop/controller/HomeController.java
+++ src/main/java/com/example/myshop/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.example.myshop.controller;
 
+import com.example.myshop.model.Item;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.math.BigDecimal;
 
 @Controller
 public class HomeController {
@@ -10,6 +13,31 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/pencil")
+    @ResponseBody
+    public Item pencil() {
+        return new Item(
+                "Ołówek",
+                new BigDecimal("1.5"),
+                "https://iheartcraftythings.com/wp-content/uploads/2021/05/Pencil-DRAWING-%E2%80%93-STEP-10.jpg"
+        );
+    }
+
+    @GetMapping("/addItem")
+    public String addItem() {
+        return "addItem";
+    }
+
+    @GetMapping("/summary")
+    public String summary() {
+        return "summary";
+    }
+
+    @GetMapping("/cartView")
+    public String cartView() {
+        return "cartView";
     }
 
 }
---
+++ src/main/java/com/example/myshop/model/Item.java
@@ -0,0 +1,20 @@
+package com.example.myshop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Item {
+
+    private String name;
+    private BigDecimal price;
+    private String imgUrl;
+
+}
